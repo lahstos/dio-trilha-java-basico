3752a3e28e6d29b1622c56c7f5c5472ecf4900f4
Larissa Santos <larissa.santos1359@edu.udesc.br>
2023-01-15 20:20:40 -0300

    Ajustando o nome da pasta

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,9 +1,11 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        int numero = 5;
+        
+        System.out.println(- numero);
 
-        System.out.print(nomeCompleto);
+        System.out.println(numero);
 
     }   
 }
