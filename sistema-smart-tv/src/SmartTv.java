cf5d65d09786cbb55a92879432453267e354566a
Larissa Santos <larissa.santos1359@edu.udesc.br>
2023-01-22 08:29:39 -0300

    Exercício de fixação

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,10 +1,12 @@
 public class SmartTv {
+
+  
        boolean ligada=false;
        int canal= 1;
        int volume = 25;
 
          public void mudarCanal (int novoCanal){
-            canal = novoCanal;
+           canal = novoCanal;
          }
 
          public void aumentarCanal(){
